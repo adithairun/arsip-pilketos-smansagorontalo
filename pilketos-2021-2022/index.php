928b0434d5f19f8837f346fbd5699464757bb8e3
Aditya Nugraha <adityanugraha795@gmail.com>
2022-10-19 05:24:26 +0800

    up

---
+++ pilketos-2021-2022/index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="Template Mo">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+  <title>PILKETOS SMAN 1 GORONTALO</title>
+  <!--ART FACTORYhttps://templatemo.com/tm-537-art-factory-->
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/templatemo-art-factory.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/owl-carousel.css">
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <!-- ***** Preloader Start ***** -->
+  <div id="preloader">
+    <div class="jumper">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div> <!-- ***** Preloader End ***** -->
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** --> <a href="#" class="logo"></a> <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <!--   <ul class="nav">                            <li class="scroll-to-section"><a href="#welcome" class="active">Home</a></li>                            <li class="scroll-to-section"><a href="#about">About</a></li>                            <li class="scroll-to-section"><a href="#services">Services</a></li>                            <li class="scroll-to-section"><a href="#frequently-question">Frequently Questions</a></li>                            <li class="submenu">                                <a href="javascript:;">Drop Down</a>                                <ul>                                    <li><a href="">About Us</a></li>                                    <li><a href="">Features</a></li>                                    <li><a href="">FAQ's</a></li>                                    <li><a href="">Blog</a></li>                                </ul> -->
+            </li>
+            </ul> <a class='menu-trigger'> <span>Menu</span> </a> <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header> <!-- ***** Header Area End ***** -->
+  <!-- ***** Welcome Area Start ***** -->
+  <div class="welcome-area" id="welcome">
+    <!-- ***** Header Text Start ***** -->
+    <div class="header-text">
+      <div class="container">
+        <div class="row">
+          <div class="left-text col-lg-6 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+		  
+		 
+		  <br>
+		  <br>
+		  <br>
+		  <br>
+		  <br>
+		  <br>
+
+            
+            <font color="black">
+            <h1 class="badge badge-primary" style="color: white;">PILKETOS</h1> <br><h1 class="badge badge-primary" style="color: white;">SMAN 1 GORONTALO</h1>
+              <h5> </h5>
+            </font> <br> 
+			
+            <a class="btn btn-success btn-lg" href="ketos/index.php/user/login"><i class="fa fa-users"></i> LOGIN SISWA</a>
+            <!-- <button id="Pilketos Belum Dibuka" class="btn btn-success btn-lg" onClick="reply_click()"> <i class="fa fa-users"></i> LOGIN SISWA</button>    
+             -->
+            <br>
+              <br>
+                
+                <a class="btn btn-dark btn-lg" href="ketos/index.php/guru/login"><i class="fa fa-university"></i> LOGIN GURU DAN PEGAWAI</a>
+                <!-- <button id="Pilketos Belum Dibuka" class="btn btn-success btn-lg" onClick="reply_click()"> <i class="fa fa-university"></i> LOGIN GURU DAN PEGAWAI</button>    
+                -->
+<script>
+function reply_click()
+{
+    alert(event.srcElement.id);
+}
+  </script>
+                <font color="black">
+
+
+              <h5>Sisa Waktu Untuk Memilih</h5>
+              <h5 id="demo"></h5>
+            </font> <br> <br>
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 21, 2021 15:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML =  hours + " Jam "
+  + minutes + " Menit " + seconds + " Detik ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "PILKETOS SMAN 1 GORONTALO TELAH DITUTUP.";
+  }
+}, 1000);
+</script>
+			
+	
+			  <button class="btn btn-danger btn-lg active" role="button" data-toggle="modal" data-target="#exampleModal" aria-pressed="true"> <i class="fa fa-question-circle"></i> Bantuan</button>
+			  <!-- <a href="hasilpilketos.png" class="btn btn-success btn-lg active" role="button">Cek Hasil PILKETOS</a> -->
+          </div>
+				
+        </div>
+      </div>
+    </div> <!-- ***** Header Text End ***** -->
+  </div> <!-- ***** Welcome Area End ***** -->
+  
+  
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Kontak Bantuan</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <table class="table table-striped">
+  
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Nama</th>
+      <th scope="col">No WA</th>
+      <th scope="col">Klik Untuk WA Panitia</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Grace</td>
+       <td>0897-9499-586</td>
+      <td><a href="https://wa.me/628979499586">WA Grace</a></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Gina</td>
+      <td>0813-4150-2250</td>
+      <td><a href="https://wa.me/6281341502250">WA Gina</a></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Divia</td>
+      <td>0823-4870-8281</td>
+      <td><a href="https://wa.me/6282348708281">WA Divia</a></td>
+    </tr>
+  </tbody>
+</table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+  <!-- <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s"> <img src="assets/images/slider-icon.png" class="rounded img-fluid d-block mx-auto" alt="First Vector Graphic"> </div> -->
+  <section class="section" id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 col-md-12 col-sm-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s"> <img src="assets/images/1-2021.jpeg" class="rounded img-fluid d-block mx-auto" alt="App"> </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="hr"></div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 col-md-12 col-sm-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s"> <img src="assets/images/2-2021.jpeg" class="rounded img-fluid d-block mx-auto" alt="App"> </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="hr"></div>
+        </div>
+      </div>
+    </div>
+   
+    	<!-- <div class="container">            <div class="row">                <div class="col-lg-7 col-md-12 col-sm-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">                    <img src="assets/images/left-image.png" class="rounded img-fluid d-block mx-auto" alt="App">                </div>                           </div>            <div class="row">                <div class="col-lg-12">                    <div class="hr"></div>                </div>            </div>        </div>
+   -->
+  </section>
+  <!--    <section class="section" id="contact-us">        <div class="container-fluid">            <div class="row">                                <div class="col-lg-6 col-md-6 col-sm-12">                    <div id="map">                                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.644358165334!2d123.05921861470055!3d0.5350446996096553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32792ad836542ebf%3A0xc18e5608ec0abddd!2sSMA%20Negeri%201%20Gorontalo!5e0!3m2!1sid!2sid!4v1586009977187!5m2!1sid!2sid" width="100%" height="500px" frameborder="0" style="border:0" allowfullscreen></iframe>                    </div>                </div>                            </div>        </div>    </section> -->
+  <!-- ***** Contact Us End ***** -->
+  <!-- ***** Footer Start ***** -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 col-md-12 col-sm-12">
+          <p class="copyright">Copyright &copy; 2020 - <?php echo date("Y") ?><br> <a rel="nofollow" href="http://tim-it.sma1gorontalo.sch.id">TIM IT SMAN 1 GORONTALO</a> <!--   . Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p> --> <br>
+          <i class="fa fa-code"> With <i class="fa fa-coffee"> </i> & </i> <i class="fa fa-heart"> </i> By <a href="https://instagram.com/adithairun">Aditya Hairun</a>
+        </div>
+        <div class="col-lg-5 col-md-12 col-sm-12">
+          <ul class="social">
+		  
+		   <li><a href="http://instagram.com/hubmas.smansagorontalo"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="http://facebook.com/hubmas.smansagtlo"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="http://youtube.com/c/SMANEGERI1GORONTALO"><i class="fa fa-youtube"></i></a></li>
+            <li><a href="http://sma1gorontalo.sch.id"><i class="fa fa-globe"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer> <!-- jQuery -->
+  <script src="assets/js/jquery-2.1.0.min.js"></script> <!-- Bootstrap -->
+  <script src="assets/js/popper.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script> <!-- Plugins -->
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/scrollreveal.min.js"></script>
+  <script src="assets/js/waypoints.min.js"></script>
+  <script src="assets/js/jquery.counterup.min.js"></script>
+  <script src="assets/js/imgfix.min.js"></script> <!-- Global Init -->
+  <script src="assets/js/custom.js"></script>
+</body>
+
+</html>
